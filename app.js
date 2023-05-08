ea3c1c82347de42a8312b5fb9919359f7e5d3459
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-05-08 19:26:46 +0300

    new server

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('Express Tutorial')
+const { createServer } = require('http');
+
+const server = createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'content-type': 'text/html' });
+    res.write('<h1>1234</h1>');
+    res.end();
+  } else if (req.url === '/about') {
+    res.writeHead(200, { 'content-type': 'text/html' });
+    res.write('<h1>5678</h1>');
+    res.end();
+  } else {
+    res.writeHead(404, 'Not found', { 'content-type': 'text/plain' });
+    res.write('OOO');
+    res.end();
+  }
+});
+
+server.listen(5000);
