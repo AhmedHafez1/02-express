e8e64d1df2d118468f2189db6fddea747d9bf421
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-05-08 22:07:06 +0300

    serving html with style and js

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
 const { createServer } = require('http');
 const { readFileSync } = require('fs');
 
-const homePage = readFileSync('./index.html', 'utf8');
+const homePage = readFileSync('./nice-app/index.html', 'utf8');
+const homeStyle = readFileSync('./nice-app/style.css', 'utf8');
+const homeLogic = readFileSync('./nice-app/script.js', 'utf8');
 
 const server = createServer((req, res) => {
   if (req.url === '/') {
     res.writeHead(200, { 'content-type': 'text/html' });
     res.write(homePage);
     res.end();
-  } else if (req.url === '/main') {
-    res.writeHead(200, { 'content-type': 'text/html' });
-    res.write('<h1>1234</h1>');
+  } else if (req.url === '/style.css') {
+    res.writeHead(200, { 'content-type': 'text/css' });
+    res.write(homeStyle);
     res.end();
-  } else if (req.url === '/about') {
-    res.writeHead(200, { 'content-type': 'text/html' });
-    res.write('<h1>5678</h1>');
+  } else if (req.url === '/script.js') {
+    res.writeHead(200, { 'content-type': 'text/javascript' });
+    res.write(homeLogic);
     res.end();
   } else {
     res.writeHead(404, 'Not found', { 'content-type': 'text/plain' });
-    res.write('OOO');
+    res.write('Not Found');
     res.end();
   }
 });
---
+++ nice-app/script.js
@@ -0,0 +1,6 @@
+// Add event listener to button
+const homeLink = document.querySelector('.home-btn');
+homeLink.addEventListener('click', () => {
+  console.log('Home clicked');
+  alert('Thanks for clicking me!');
+});
