5de1763fbe3a63f72e21443a3eb1472a0e82704e
EMEA\IBA1SF <fixed-term.ahmad.ibraheem@bosch.com>
2023-05-10 15:24:34 +0300

    usinf request parameters

--- app.js
+++ app.js
@@ -2,8 +2,21 @@ const { products, people } = require("./data");
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+  res.status(200).send('<h2><a href="/api/products">Products API</a></h2>');
+});
+
 app.get("/api/products", (req, res) => {
   res.status(200).json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const { id } = req.params;
+  const product = products.find((p) => p.id === +id);
+
+  if (!product) res.status(404).send("<h2>Product does not exist</h2>");
+
+  res.status(200).json(product);
 });
 
 app.listen(5000);
--- basic-static-server.js
+++ basic-static-server.js
@@ -1,23 +1,23 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 const app = express();
 
-const staticPath = path.resolve(__dirname, './public');
-const homePath = path.resolve(__dirname, './navbar-app/index.html');
-const aboutPath = path.resolve(__dirname, './nice-app/index.html');
+const staticPath = path.resolve(__dirname, "./public");
+const homeFile = path.resolve(__dirname, "./navbar-app/index.html");
+const aboutFile = path.resolve(__dirname, "./nice-app/index.html");
 
 app.use(express.static(staticPath));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(homePath));
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(homeFile));
 });
 
-app.get('/about.html', (req, res) => {
-  res.sendFile(aboutPath);
+app.get("/about.html", (req, res) => {
+  res.sendFile(aboutFile);
 });
 
-app.all('*', (req, res) => {
-  res.status(404).send('Resource not found!');
+app.all("*", (req, res) => {
+  res.status(404).send("Resource not found!");
 });
 
-app.listen(5000, () => console.log('Server is listening on port 5000'));
+app.listen(5000, () => console.log("Server is listening on port 5000"));
