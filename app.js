02f87de43a98025421da4ea8b880a2f355838477
EMEA\IBA1SF <fixed-term.ahmad.ibraheem@bosch.com>
2023-05-11 10:28:12 +0300

    possting with forms and fe

--- app.js
+++ app.js
@@ -8,17 +8,34 @@ const morgan = require("morgan");
 // app.use('/api', logger);
 
 app.use(morgan("tiny"));
-
-app.get("/", (req, res) => {
-  res.status(200).send("Welcome");
-});
+app.use(express.static("./methods-public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.get("/api/products", (req, res) => {
   res.status(200).json(products);
 });
 
-app.get("/api/orders", (req, res) => {
-  res.send("Orders");
+app.post("/login", (req, res) => {
+  const { name } = req?.body;
+  if (name) {
+    res.status(200).send("Logged in successfully");
+  } else {
+    res.status(401).send("Unauthorized");
+  }
+});
+
+app.get("/api/people", (req, res) => {
+  res.json({ success: true, data: people });
+});
+
+app.post("/api/people", (req, res) => {
+  const { name } = req.body;
+
+  if (name) {
+    res.status(201).json({ success: true, person: name });
+  }
+  res.status(400).json({ success: false, msg: "Noo Name" });
 });
 
 app.get("/api/user", (req, res) => {
