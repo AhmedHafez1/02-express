a6e6eb7fdad325df4e0bb8bf05920cad47165f7e
EMEA\IBA1SF <fixed-term.ahmad.ibraheem@bosch.com>
2023-05-11 08:45:46 +0300

    third party middleware

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
-const { products, people } = require('./data');
-const express = require('express');
+const { products, people } = require("./data");
+const express = require("express");
 const app = express();
-const logger = require('./logger');
-const auth = require('./auth');
+const logger = require("./logger");
+const auth = require("./auth");
+const morgan = require("morgan");
 
-app.use('/api', logger);
+// app.use('/api', logger);
 
-app.get('/', (req, res) => {
-  res.status(200).send('Welcome');
+app.use(morgan("tiny"));
+
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome");
 });
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
+app.get("/api/products", (req, res) => {
+  res.status(200).json(products);
 });
 
-app.get('/api/orders', (req, res) => {
-  res.send('Orders');
+app.get("/api/orders", (req, res) => {
+  res.send("Orders");
 });
 
-app.get('/api/user', auth, (req, res) => {
+app.get("/api/user", (req, res) => {
   res.json(req.user);
 });
 
